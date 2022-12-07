d578487ab61a01dfe54c5bf66f2e4e62a70ded9f
TejaReddyK <119155388+TejaReddyK@users.noreply.github.com>
2022-12-07 17:25:05 +0530

    Create prime.js

---
+++ dec6/JS/prime.js
@@ -0,0 +1,16 @@
+function prime() {
+      for (let i = 2; i <= 100; i++) {
+        let prime = true;
+            for (let j = 2; j < i; j++) {
+             if (i % j == 0) {
+             prime = false;
+             break;
+           }
+        }
+       if (prime) {
+        console.log(i);
+        }
+    }
+}
+prime();
+
